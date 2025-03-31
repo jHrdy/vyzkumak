3ab9d1f86d7b0571f31e5b4f35873d313d5d0980
jhrdy <jozef.hrdy@cern.ch>
2025-03-31 16:52:21 +0200

    adding visual for histograms

---
+++ inspect_hist.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from ready_data import norm_data
+
+def plot_hist(hist_num):
+
+    if hist_num < 0:
+        return False
+    
+    plt.figure(figsize=(10, 5))
+    plt.bar(x:=range(20), norm_data[hist_num], color='blue', alpha=0.7)
+
+    plt.xlabel("Bin")
+    plt.ylabel("Value")
+    plt.title(f"Histogram plot for pt. {hist_num}")
+    plt.xticks(x) 
+
+    plt.show()
+    plt.show()
+    return True
+
+if __name__ == "__main__":
+    print(f'Keep entering integers in range 0 to {len(norm_data.T)}, to exit input negative integer')
+    while True:
+       if plot_hist(int(input("Type number of desired histogram: "))):
+           continue
+       else:
+           break
