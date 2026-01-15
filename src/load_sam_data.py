fbfd68e73ee0c6e1bb43440b9bcbd7157aa977b9
Jozef Hrdy <hrdyjoze@cvut.cz>
2026-01-15 14:58:27 +0100

    benchmarks on datasets done

--- src/load_sam_data.py
+++ src/load_sam_data.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import numpy as np
+import os
 
 datasets = {
     "SI" : "..\\data\\SI\\SI01U1\\SI01U1_ch",
@@ -10,13 +11,18 @@ datasets = {
 def load_dataset(dataset_name):
 
     if dataset_name not in datasets.keys():
-        raise KeyError(f"No dataset names {dataset_name}")
+        raise KeyError(f"No dataset names {dataset_name} did you mean: {datasets.keys()}?")
     else:
-        dataset_dir = Path(datasets[dataset_name])
+        if not 'data' in os.listdir():
+            dataset_dir = Path(datasets[dataset_name])
+        else:
+            dataset_dir = Path(datasets[dataset_name].lstrip("..\\"))
+
     print(dataset_dir)
     files = sorted(dataset_dir.glob("*.npz"))
+    
     if not files:
-        raise RuntimeError("No .npz files found")
+            raise RuntimeError("No .npz files found")
     
     dataset = []
     for f in files:
