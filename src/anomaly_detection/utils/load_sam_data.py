2ffe0ff10f256ff6fd84e3685927e1d036d5f7bb
Jozef Hrdy <hrdyjoze@cvut.cz>
2026-02-20 10:50:07 +0100

    major restructure

---
+++ src/anomaly_detection/utils/load_sam_data.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import numpy as np
+import os
+import anomaly_detection.config.paths as paths
+
+datasets = {
+    "SI" : f"{paths.DATA_DIR}\\SI\\SI01U1\\SI01U1_ch",
+    "FJ" : f"{paths.DATA_DIR}\\FJ\\FI01X1\\FI01X1_ch",
+    "PA" : f"{paths.DATA_DIR}\\MWPC\\PA01U1\\PA01U1_ch"
+}
+
+def load_dataset(dataset_name):
+
+    if dataset_name not in datasets.keys():
+        raise KeyError(f"No dataset names {dataset_name} did you mean: {datasets.keys()}?")
+    else:
+        if not 'data' in os.listdir():
+            dataset_dir = Path(datasets[dataset_name])
+        else:
+            dataset_dir = Path(datasets[dataset_name].lstrip("..\\"))
+
+    print("Loading data from:", dataset_dir)
+
+    files = sorted(dataset_dir.glob("*.npz"))
+    
+    if not files:
+            raise RuntimeError("No .npz files found")
+    
+    dataset = []
+    for f in files:
+        sample = np.load(f)
+        dataset.append(sample['values'])
+    
+    return dataset
